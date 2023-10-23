21b120b9f72e3f398d30500044e5d70a82cb504d
AnandhSrinivasan <devanandh2902@gmail.com>
2023-10-23 14:26:14 +0530

    New Commit

--- Task20/src/test/java/org/Task20/Selenium/Guvi_Signup_Login.java
+++ Task20/src/test/java/org/Task20/Selenium/Guvi_Signup_Login.java
@@ -12,21 +12,24 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Guvi_Signup_Login {
 
-	static WebDriver driver;
+	// static WebDriver driver;
+
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		
-		//Step 1 launch the browser and Navigate to Given URL
-		driver = new ChromeDriver();
+
+		// Step 1 launch the browser and Navigate to Given URL
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://www.guvi.in/");
 		driver.manage().window().maximize();
 		Timeouts handletime = driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
-		//Step 2 Click the SignUp Button
-		WebElement signupbutton= driver.findElement(By.xpath("//a[@class='nav-link btn btn-primary text-white px-4']"));
+
+		// Step 2 Click the SignUp Button
+		WebElement signupbutton = driver
+				.findElement(By.xpath("//a[@class='nav-link btn btn-primary text-white px-4']"));
 		signupbutton.click();
-		
-		//Step 3 and 4 Fill up the SignUp details and Click SignUpbutton
+
+		// Step 3 and 4 Fill up the SignUp details and Click SignUpbutton
 		WebElement firstname = driver.findElement(By.xpath("//input[@id='firstName']"));
 		firstname.sendKeys("Deva");
 		WebElement lastname = driver.findElement(By.xpath("//input[@id='lastName']"));
@@ -37,54 +40,53 @@ public class Guvi_Signup_Login {
 		password.sendKeys("Batman111@#");
 		WebElement mobileNum = driver.findElement(By.xpath("//input[@id='mobileNumberInput']"));
 		mobileNum.sendKeys("8125499998");
-		WebElement signupbutton1= driver.findElement(By.xpath("//button[@id='signup']"));
+		WebElement signupbutton1 = driver.findElement(By.xpath("//button[@id='signup']"));
 		signupbutton1.click();
-		
+
 		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		//Step 5 Verify Success Registered
+
+		// Step 5 Verify Success Registered
 		WebElement displaymessage = driver.findElement(By.xpath("//h2[text()='Successfully Registered']"));
 		displaymessage.getText();
-		if (displaymessage.equals("Successfully Registered")) 
-		{
+		if (displaymessage.equals("Successfully Registered")) {
 			System.out.println("Registration is Success");
 		} else {
 			System.out.println("Registration is UnSuccess");
 		}
-		
-		//Step 6 Login Functions 
+
+		// Step 6 Login Functions
 		WebElement proceedsignin = driver.findElement(By.xpath("//a[@id='status_proceed']"));
 		proceedsignin.click();
-		WebElement emailbox =  driver.findElement(By.id("login_email"));
-		emailbox .sendKeys("11test@gmail.com");
+		WebElement emailbox = driver.findElement(By.id("login_email"));
+		emailbox.sendKeys("11test@gmail.com");
 		Thread.sleep(5000);
-		WebElement passwordbox =  driver.findElement(By.xpath("//input[@id='login_password']"));
+		WebElement passwordbox = driver.findElement(By.xpath("//input[@id='login_password']"));
 		passwordbox.sendKeys("Batman111@#");
 		Thread.sleep(5000);
-		WebElement loginbutton =  driver.findElement(By.id("login_button"));
+		WebElement loginbutton = driver.findElement(By.id("login_button"));
 		loginbutton.click();
-		
+
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		//Step 7 Verify User Login Successfully
+
+		// Step 7 Verify User Login Successfully
 		String title = driver.getTitle();
-		if(title.contains("courses")) {
-			System.out.println("Login Successfull");
-		}else {
-			System.out.println("Login UnSuccessfull");
+		if (title.contains("courses")) {
+			System.out.println("Login was Successfull");
+		} else {
+			System.out.println("Login was UnSuccessfull");
 		}
-		
-		//Step 8
+
+		// Step 8
 		driver.close();
 	}
 
--- Task20/src/test/java/org/Task20/Selenium/JQuery_Date.java
+++ Task20/src/test/java/org/Task20/Selenium/JQuery_Date.java
@@ -5,43 +5,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class JQuery_Date {
 
-	static WebDriver driver;
+	// static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		
-		//Step1 Launch Browser, Maximize it, Navigate to URL
-	    driver = new ChromeDriver();
+
+		// Step1 Launch Browser, Maximize it, Navigate to URL
+		WebDriver driver = new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
 		driver.manage().window().maximize();
 		driver.navigate().to("https://jqueryui.com/datepicker/");
-		//Thread.sleep(2000);
-		
-		//Step2 Switch to iFrame
+		// Thread.sleep(2000);
+
+		// Step2 Switch to iFrame
 		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
 		driver.switchTo();
-		//Thread.sleep(4000);
-		
-		//Step3 Select Next Month
-		
-		//A.Click the date picker
-		WebElement dateInput =  driver.findElement(By.xpath("//input[@id='datepicker']"));
-		dateInput .click();
-		//B.Click the Next to change the Month
+		// Thread.sleep(4000);
+
+		// Step3 Select Next Month
+
+		// A.Click the date picker
+		WebElement dateInput = driver.findElement(By.xpath("//input[@id='datepicker']"));
+		dateInput.click();
+		// B.Click the Next to change the Month
 		driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).click();
-		//Thread.sleep(4000);
-		
-		//Step4 Select Date 22 from the Next Month
-		WebElement date=  driver.findElement(By.xpath("//a[text()='22']"));
+		// Thread.sleep(4000);
+
+		// Step4 Select Date 22 from the Next Month
+		WebElement date = driver.findElement(By.xpath("//a[text()='22']"));
 		date.click();
 		String printthedate = dateInput.getAttribute("value");
-		//Thread.sleep(4000);
-		
-		//Step6 Print the Selected date in Console
+		// Thread.sleep(4000);
+
+		// Step6 Print the Selected date in Console
 		System.out.println("Selected date is : " + printthedate);
-		//Thread.sleep(5000);
-		
-		//Step 5 Close the browser
+		// Thread.sleep(5000);
+
+		// Step 5 Close the browser
 		driver.close();
 		driver.quit();
 
